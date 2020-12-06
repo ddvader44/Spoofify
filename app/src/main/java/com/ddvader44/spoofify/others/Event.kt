2fce168ed1a676e4959434e30537c7894fe665b4
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-12-07 02:09:16 +0530

    gdg event and resource class

---
+++ app/src/main/java/com/ddvader44/spoofify/others/Event.kt
@@ -0,0 +1,18 @@
+package com.ddvader44.spoofify.others
+
+open class Event<out T>(private val data : T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled() : T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            data
+        }
+    }
+
+    fun peekContent() = data
+}
