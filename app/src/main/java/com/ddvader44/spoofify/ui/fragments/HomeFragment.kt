40d659ba21d68ba216850f1286e20838a19fc083
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-12-10 02:48:49 +0530

    home fragment

---
+++ app/src/main/java/com/ddvader44/spoofify/ui/fragments/HomeFragment.kt
@@ -0,0 +1,63 @@
+package com.ddvader44.spoofify.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ddvader44.spoofify.R
+import com.ddvader44.spoofify.adapters.SongAdapter
+import com.ddvader44.spoofify.others.Status
+import com.ddvader44.spoofify.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home){
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var songAdapter: SongAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        setupRecyclerView()
+        subscribeToObservers()
+
+        songAdapter.setOnItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
+    }
+
+    private fun setupRecyclerView() = rvAllSongs.apply{
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
+            when(result.status) {
+                Status.SUCCESS -> {
+                    allSongsProgressBar.isVisible = false
+                    result.data?.let { songs ->
+                        songAdapter.songs = songs
+                    }
+
+                }
+                Status.ERROR -> Unit
+                Status.LOADING -> allSongsProgressBar.isVisible = true
+            }
+        }
+    }
+
+
+
+
+
+
+}
